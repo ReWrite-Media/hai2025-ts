233c03519e18e4114dd41b439b51acf6082973ec
neonerz <gerald@rewrite.media>
2025-09-15 23:04:11 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -2,12 +2,28 @@
 // Custom MakeCode extension file
 
 //% weight=200 color="#0000FF" icon="brain" block="Hour of AI"
-namespace Hour_of_AI {
+namespace hai {
     /**
-     * Returns a greeting message.
+     * Select Oak Log
      */
-    //% block
-    export function greet(name: string): string {
-        return `Hello, ${name}!`;
+    //% block = "`block.LogOak` Oak Log"
+    export function logOak(): void{
+        player.execute("scoreboard players set .output global 1")
     }
+
+    /**
+     * Select Oak Log
+     */
+    //% block = "`block.LogBirch` Oak Log"
+    export function logBirch(): void{
+        player.execute("scoreboard players set .output global 2")
+    }
+    
+    /**
+     * Select Oak Log
+     */
+    //% block = "`block.LogAcacia` Oak Log"
+    export function logAcacia(): void{
+        player.execute("scoreboard players set .output global 3")
+    }    
 }
