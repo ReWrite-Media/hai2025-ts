c56ce708190137d9e9cacf0d05c5fedf8316e875
neonerz <gerald@rewrite.media>
2025-09-15 23:55:24 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -7,23 +7,35 @@ namespace hai {
      * Select Oak Log
      */
     //% block = "`Block.LogOak` oak log"
-    export function logOak(): void {
-        player.execute("scoreboard players set .output global 1")
+    export function logOak(): number {
+        return 1
     }
 
     /**
      * Select Birch Log
      */
     //% block = "`Block.LogBirch` birch log"
-    export function logBirch(): void {
-        player.execute("scoreboard players set .output global 2")
+    export function logBirch(): number {
+        return 2
     }
 
     /**
      * Select Acacia Log
      */
     //% block = "`Block.LogAcacia` acacia log"
-    export function logAcacia(): void {
-        player.execute("scoreboard players set .output global 3")
+    export function logAcacia(): number {
+        return 3
     }
+
+    /**
+     * Select Oak Log
+     */
+    //% block = "classify wood as %n"
+    export function _classifyWood(n: number): void {
+        player.execute(`scoreboard players set .output global ${n}`);
+    }
+
+
+
+
 }
