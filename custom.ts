b33df5306050d409287000238847192e29af5e63
neonerz <gerald@rewrite.media>
2025-09-16 14:48:53 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,13 +1,24 @@
 // custom.ts
 // Custom MakeCode extension file
 
-//% weight=200 color="#0096FF" icon="brain" block="Hour of AI"
-namespace hai {
+
+//% weight=200 color="#0096FF" icon="\uf126" block="HAI Actions"
+namespace actions {
+    /**
+     * Classify a wood log.
+     */
+    //% block = "classify %n as wood `custom.logOak"
+    export function classifyWood(n: number): void {
+        player.execute(`scoreboard players set .output${n} global ${n}`);
+    }
+}
+
+//% weight=200 color="#008106" icon="\uf126" block="HAI Values"
+namespace values {
     /**
      * Select Oak Log
      */
     //% block = "`custom.logOak` oak log"
-    //% blockSetVariable = "logType"
     export function logOak(): number {
         return 1;
     }
@@ -16,7 +27,6 @@ namespace hai {
      * Select Birch Log
      */
     //% block = "`custom.logBirch` birch log"
-    //% blockSetVariable = "logType"
     export function logBirch(): number {
         return 2;
     }
@@ -25,17 +35,8 @@ namespace hai {
      * Select Acacia Log
      */
     //% block = "`custom.logAcacia` acacia log"
-    //% blockSetVariable = "logType"
     export function logAcacia(): number {
         return 3;
-    }
-
-    /**
-     * Classify a wood log.
-     */
-    //% block = "classify %n as wood"
-    export function classifyWood(n: number): void {
-        player.execute(`scoreboard players set .output${n} global ${n}`);
     }
 
 }
