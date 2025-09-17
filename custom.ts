e2f5513f58a3f8ed32a2d3a4412c30a7a7687827
neonerz <gerald@rewrite.media>
2025-09-17 01:50:53 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -62,10 +62,10 @@ namespace hai {
     /**
      * Ghost Grid.
      */
-    //% blockId=ghostPattern block=" "
-    //% imageLiteralColumns=0
-    //% imageLiteralRows=0
-    //% gridLiteral=0
+    //% blockId=ghostPattern block=""
+    //% imageLiteralColumns=2
+    //% imageLiteralRows=2
+    //% gridLiteral=1
     //% color="#8E8E8E"    
     export function ghostPattern(pattern: string) {
         return new CraftPattern(" ");
